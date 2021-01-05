101f4e6440445e6b9a706edc143495bf1cc2b61f
Marvin T <contact@marvinkturner.com>
2021-01-05 00:49:57 -0500

    fix: reference correct prop for data

--- src/List.js
+++ src/List.js
@@ -4,7 +4,7 @@ const List = ({ people }) => {
   return (
     <>
       {people.map((person) => {
-        const { id, name, age, image  } = people;
+        const { id, name, age, image } = person;
         return (
           <article key={id} className='person'>
             <img src={image} alt={name} />
--- src/data.js
+++ src/data.js
@@ -1,35 +1,35 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
+    name: 'Brittany Yates',
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
+    name: 'Janae Garrett',
     age: 32,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
-    name: 'Larry Little',
+    name: 'Andre Okafor',
     age: 36,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
-    name: 'Sean Walsh',
+    name: 'Steve Wahlberg',
     age: 34,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
   {
     id: 5,
-    name: 'Lola Gardner',
+    name: 'Lyla Hurtz',
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
